0dbfa9a81c70d0616b7234cdb463f352ef0ffd3f
Damian Walendzik <Damianwalendzik7@gmail.com>
2024-02-01 21:37:46 +0100

    some functionalities related to filters added

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
+from .models import Item, Category 
 from django.contrib.auth.decorators import login_required
 from .forms import NewItemForm, EditItemForm
+from django.db.models import Q
 
 # Create your views here.
 def detail(request, pk):
@@ -57,8 +58,20 @@ def edit(request, pk):
     })
 
 def items(request):
+    query = request.GET.get('query', '')
     items = Item.objects.filter(is_sold=False)
+    categories = Category.objects.all()
+    category_id = request.GET.get('category',0)
+
+    if category_id:
+        items=items.filter(category_id=category_id)
+
+    if query:
+        items=items.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
     return render(request, 'item/items.html', {
-        'items':items
+        'items':items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
     })
